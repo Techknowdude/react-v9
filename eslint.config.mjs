404f95edb5118074554da9f152a9dc25485a2c4a
Techknowdude <bwestmoreland42@gmail.com>
2026-06-05 09:51:38 -0700

    Up to the vite lesson

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,11 +2,11 @@ import js from "@eslint/js";
 import globals from "globals";
 import prettier from "eslint-config-prettier";
 
-/** @type {import('eslint').Linter.Config} */
+/** @type {import('eslint').Linter.Config[]} */
 export default [
   js.configs.recommended,
   {
-    files: ["**/*.js", "**/*.jsx"],
+    files: ["**/*.js"],
     languageOptions: {
       globals: { ...globals.browser, ...globals.node },
       parserOptions: {
