6ccb9728eefb61d5280b10c792f03817e935159e
Techknowdude <bwestmoreland42@gmail.com>
2026-06-12 20:14:51 -0700

    Adding past orders - cached queries using TanQuery

--- src/routes/past.lazy.jsx
+++ src/routes/past.lazy.jsx
@@ -1,5 +1,51 @@
+import { useState } from "react";
+import { useQuery } from "@tanstack/react-query";
 import { createLazyFileRoute } from "@tanstack/react-router";
+import getPastOrders from "../api/getPastOrders";
 
 export const Route = createLazyFileRoute("/past")({
-  component: () => <div>Hello /past!</div>,
+  component: PastOrdersRoute,
 });
+
+function PastOrdersRoute() {
+  const [page, setPage] = useState(1);
+  const { isLoading, data } = useQuery({
+    queryKey: ["pastOrders", page],
+    queryFn: () => getPastOrders(page),
+    staleTime: 1000 * 30, // 30 seconds
+  });
+  if (isLoading) {
+    return <div className="past-orders">Loading...</div>;
+  }
+  return (
+    <div className="past-orders">
+      <table>
+        <thead>
+          <tr>
+            <td>ID</td>
+            <td>Date</td>
+            <td>Time</td>
+          </tr>
+        </thead>
+        <tbody>
+          {data.map((order) => (
+            <tr key={order.order_id}>
+              <td>{order.order_id}</td>
+              <td>{order.date}</td>
+              <td>{order.time}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      <div className="pages">
+        <button disabled={page <= 1} onClick={() => setPage(page - 1)}>
+          Previous
+        </button>
+        <div>{page}</div>
+        <button disabled={data.length < 10} onClick={() => setPage(page + 1)}>
+          Next
+        </button>
+      </div>
+    </div>
+  );
+}
